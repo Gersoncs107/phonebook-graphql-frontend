b3b1a0494abad2623b9de2502268ac0a7a0bc163
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-01 22:26:27 -0300

    phonebook-graphql-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client'
+import {ApolloProvider} from '@apollo/client/react'
 
 const client = new ApolloClient({
   link: new HttpLink({
