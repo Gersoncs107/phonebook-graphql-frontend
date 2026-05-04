0bf5d319e6d640257e1064a1793913e8282c306b
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-03 22:43:10 -0300

    phonebook-graphql-frontend-update

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -16,6 +16,25 @@ const FIND_PERSON = gql`
   }
 `
 
+const CREATE_PERSON = gql`
+  mutation createPerson(
+    $name: String!
+    $street: String!
+    $city: String!
+    $phone: String
+  ) {
+    addPerson(name: $name, street: $street, city: $city, phone: $phone) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
 const Person = ({ person, onClose }) => {
   return (
     <div>
