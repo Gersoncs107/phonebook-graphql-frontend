d88d9ef6ce8f44e563ef50da57fbfa2fa57456db
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-06 22:32:16 -0300

    phonebook-graphql-frontend-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { gql } from '@apollo/client'
 import { useQuery } from '@apollo/client/react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`
+import { ALL_PERSONS } from './queries'
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS, {
-    pollInterval: 2000
-  })
+  const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
