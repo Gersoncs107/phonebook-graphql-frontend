cfd38d32ed9b5ebcec20e2ac19d643110f8a53f9
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-09 19:31:36 -0300

    phonebook-graphql-frontend-update

--- src/components/PhoneForm.jsx
+++ src/components/PhoneForm.jsx
@@ -5,4 +5,6 @@ import { EDIT_NUMBER } from "../queries";
 const PhoneForm = () => {
    const [name, setName] = useState("");
    const [phone, setPhone] = useState("");
+
+   const changeNumber = useMutation(EDIT_NUMBER);
 }
