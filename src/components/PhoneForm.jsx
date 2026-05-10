e0eacbd165ededf61ca3532c61e1810dd85643ee
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-09 21:50:46 -0300

    phonebook-graphql-frontend-update

--- src/components/PhoneForm.jsx
+++ src/components/PhoneForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client/react";
 import { EDIT_NUMBER } from "../queries";
 
-const PhoneForm = () => {
+const PhoneForm = ({setError}) => {
    const [name, setName] = useState("");
    const [phone, setPhone] = useState("");
 
