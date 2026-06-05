102ddfc75bfd687c073b836c55d48dba21d6f086
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-04 22:33:10 -0300

    phonebook-graphql-frontend-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useQuery } from '@apollo/client/react'
-import { useState } from 'react'
 import { useApolloClient, useQuery } from '@apollo/client/react'
+import { useState } from 'react'
 import LoginForm from './components/LoginForm'
 import Notify from './components/Notify'
 import PersonForm from './components/PersonForm'
