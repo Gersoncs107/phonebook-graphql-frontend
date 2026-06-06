baec799fbaa6583540dd71d8cdb0027407064f64
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-06 19:21:26 -0300

    phonebook-graphql-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -23,24 +23,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`
-
-client.query({ query }).then((response) => {
-  console.log(response.data)
-})
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ApolloProvider client={client}>
