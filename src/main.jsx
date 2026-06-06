db756a00e84a0b7ca317b511b8832bbb9279390e
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-06 19:31:09 -0300

    phonebook-graphql-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const authLink  = new SetContextLink(({ headers }) => {
   }
 })
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' }) // highlight-line
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
