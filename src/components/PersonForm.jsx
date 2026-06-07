e7f149d9f403206f9fcbc1b064f3b9d664b76df6
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-06 22:22:24 -0300

    phonebook-graphql-frontend-update

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -38,7 +38,12 @@ const PersonForm = ({ setError }) => {
   const submit = (event) => {
     event.preventDefault()
 
-    createPerson({ variables: { name, phone, street, city } })
+    createPerson({ variables: { 
+      name, 
+      phone: phone.length > 0 ? phone : undefined, 
+      street, city
+     } 
+    })
 
     setName('')
     setPhone('')
