8dd0090c969cb37915b0ff8f64596d8333b21ff5
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-21 22:29:06 -0300

    phonebook-graphql-frontend-update

--- src/queries.js
+++ src/queries.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client'
 
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone 
+    address {
+      street 
+      city
+    }
+  }
+`
+
 export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password)  {
