424d15b01d8702fcd4cb7bb3d42cda882a9c4f1f
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-28 22:39:14 -0300

    phonebook-graphql-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -28,6 +28,18 @@ const wsLink = new GraphQLWsLink(
   }),
 )
 
+const splitLink = ApolloLink.split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink),
+)
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: authLink.concat(httpLink)
