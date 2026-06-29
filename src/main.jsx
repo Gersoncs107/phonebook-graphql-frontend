66dd43d600bdcd12ad1e3babd89a8a5117f8a2a9
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-28 22:39:48 -0300

    phonebook-graphql-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -42,7 +42,7 @@ const splitLink = ApolloLink.split(
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  link: splitLink,
 })
 
 createRoot(document.getElementById('root')).render(
